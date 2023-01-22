607364ab95831fefd4de4e4a1aef1d63067404d5
Ken Wang <ken71198@hotmail.com>
2023-01-22 23:58:31 +0100

    Update _utils.py

--- scvi/nn/_utils.py
+++ scvi/nn/_utils.py
@@ -3,6 +3,12 @@ import torch
 
 def one_hot(index: torch.Tensor, n_cat: int) -> torch.Tensor:
     """One hot a tensor of categories."""
-    onehot = torch.zeros(index.size(0), n_cat, device=index.device)
-    onehot.scatter_(1, index.type(torch.long), 1)
-    return onehot.type(torch.float32)
+    loc = (torch.gt(index, n_cat-1) | torch.lt(index, 0))
+    loc = loc.reshape(-1).tolist()
+    index2 = index.clone()
+    index2[loc,:] = 0
+    onehot = torch.zeros(index2.size(0), n_cat, device=index2.device)
+    onehot.scatter_(1, index2.type(torch.long), 1)
+    onehot.type(torch.float32)
+    onehot[loc,:] = 0
+    return onehot
