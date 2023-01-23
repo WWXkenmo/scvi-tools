91a78f525a32b0a1b2d8e061ecf02ba6d02b82a4
Ken Wang <ken71198@hotmail.com>
2023-01-23 23:12:32 +0100

    Update _archesmixin.py

--- scvi/model/base/_archesmixin.py
+++ scvi/model/base/_archesmixin.py
@@ -153,11 +153,14 @@ class ArchesMixin:
         model.is_trained_ = False
         
         ### Vamp_prior currently only work on scVI model
-        #class_Name = str(model.__class__).replace("'>","")
-        #class_Name = class_Name.split(".")[3]
-        #if class_Name == "SCVI":
-        #    model.module.use_vampprior = True
-        #    model.module.add_pseudoinputs(model.module.n_input)
+        class_Name = str(model.__class__).replace("'>","")
+        class_Name = class_Name.split(".")[3]
+        if class_Name == "SCVI":
+            model.module.use_vampprior = True
+            model.module.add_pseudoinputs(model.module.n_input)
+        
+        ### regenerate the summary string
+        model._model_summary_string = model._model_summary_string.replace("use_VamPprior: False","use_VamPprior: True")
             
         return model
 
