fa133da3817320312cb6c8e2a2bed8ed94e83582
Ken Wang <ken71198@hotmail.com>
2023-01-24 09:26:26 +0100

    Update _vamp_utils.py

--- scvi/utils/_vamp_utils.py
+++ scvi/utils/_vamp_utils.py
@@ -1,6 +1,7 @@
 from torch.autograd import Variable
 import math
 import torch
+import torch.nn as nn
 
 def reparameterize(mu, logvar):
     std = logvar.mul(0.5).exp_()
@@ -18,3 +19,28 @@ def log_Normal_diag(x, mean, log_var, average=False, dim=None):
         return torch.mean( log_normal, dim )
     else:
         return torch.sum( log_normal, dim )
+    
+##################################################  
+class NonLinear(nn.Module):
+    def __init__(self, input_size, output_size, bias=True, activation=None):
+        super(NonLinear, self).__init__()
+
+        self.activation = activation
+        self.linear = nn.Linear(int(input_size), int(output_size), bias=bias)
+
+    def forward(self, x):
+        h = self.linear(x)
+        if self.activation is not None:
+            h = self.activation( h )
+
+        return h
+#################################################
+
+
+def normal_init(m, mean=0., std=0.01):
+    m.weight.data.normal_(mean, std)
+
+
+
+
+
