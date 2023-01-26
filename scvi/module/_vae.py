1d0bc27840acfaf877e0d0d38ebbabadc5e5b918
Ken Wang <ken71198@hotmail.com>
2023-01-27 00:54:21 +0100

    Update _vae.py

--- scvi/module/_vae.py
+++ scvi/module/_vae.py
@@ -472,7 +472,7 @@ class VAE(BaseLatentModeModuleClass):
         
         ###### fix, add VampPrior
         if self.use_vampprior:
-            device = self.device_record
+            device = self.device
             if cat_covs is not None:
                 cat_covs_zero = torch.zeros([self.number_vp_components,cat_covs.shape[1]])-1
                 cat_covs_zero = cat_covs_zero.to(device)
